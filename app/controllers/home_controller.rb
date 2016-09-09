5fe81b2c0b71398f4b80e9e1ba728f62200caf23
Artem Krivonozhko <artemkrivonozhko@gmail.com>
2016-09-09 10:23:32 +0800

    Add calculator at home page
    Very simple version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,24 @@
 class HomeController < ApplicationController
+
+  TIME = 6
+
   def index
-    
+    flash[:error] = nil
+
+    @expected_profit = 30
+    @actual_profit = Loan.actual_profit
+
+    @expected_payment = nil
+    @actual_payment = nil
+
+    amount = params[:amount]
+    unless amount.blank?
+      if amount.match(/^\d+(.\d+)?$/) && amount.to_f != 0
+        @expected_payment = amount.to_f * (@expected_profit / 100.0) * TIME / 12
+        @actual_payment = amount.to_f * (@actual_profit / 100.0) * TIME / 12
+      else
+        flash[:error] = 'Некорректная сумма'
+      end
+    end
   end
 end
